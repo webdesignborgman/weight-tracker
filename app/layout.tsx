c85e9db77ed3955c69ee25915d76fb9b8ced37bf
Alex Borgman <info@webdesignborgman.nl>
2025-06-23 23:24:36 +0200

    change in global.css to fix bg error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,22 @@ import './globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-export const metadata = {
-  title: 'Weight Tracker',
-};
+export const metadata = { title: 'Weight Tracker' };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="nl">
-      <body className="bg-slate-900 text-white flex flex-col min-h-screen">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@geist-ui/react@2.2.0/dist/geist-ui.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Geist+Sans&display=swap"
+        />
+      </head>
+      <body className="flex flex-col min-h-screen bg-slate-900 text-white font-sans">
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
