9adae4646285f61909e38b7715cd3a62cc3f7f82
Alex Borgman <info@webdesignborgman.nl>
2025-06-23 23:00:01 +0200

    changed useclient on layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,14 @@ import './globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+export const metadata = {
+  title: 'Weight Tracker',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="nl">
-      <body className="flex flex-col min-h-screen bg-slate-900 text-white">
+      <body className="bg-slate-900 text-white flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
