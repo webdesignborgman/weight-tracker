e637d987f2481b55a96e671bb17aa7b68bfd85ee
Alex Borgman <info@webdesignborgman.nl>
2025-06-24 15:28:04 +0200

    changed next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
